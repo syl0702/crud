08518e5c0024cd6eab9349323b31982f864fe19c
syl0702 <navyblue794@gmail.com>
2023-08-22 14:35:42 +0900

    Update 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -42,3 +42,26 @@ def delete(request, id):
     post.delete()
 
     return redirect('/index/')
+
+def edit(request, id):
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'edit.html', context)
+
+def update(request, id):
+    # 방금 수정한 데이터
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    # post = Post() => 새로운 게시물 만들 때
+    # 기존 데이터
+    post = Post.objects.get(id=id)
+    post.title = title
+    post.content = content
+    post.save()
+
+    return redirect(f'/posts/{post.id}/')
