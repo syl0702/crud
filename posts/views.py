dec3ca56206655591c4ecadd2b904bf9557a18aa
syl0702 <navyblue794@gmail.com>
2023-08-22 11:46:52 +0900

    Read(1)기능구현

--- posts/views.py
+++ posts/views.py
@@ -12,3 +12,13 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+
+def detail(request, id):
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'detail.html', context)
